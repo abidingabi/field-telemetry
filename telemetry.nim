67239e3e5b3fac9a2085d93314d5013bf55156c2
dansman805 <dansman805@gmail.com>
2019-06-29 12:50:24 -0400

    Added things, drawline still broken

--- telemetry.nim
+++ telemetry.nim
@@ -1,5 +1,6 @@
 import math
-import stats
+from strutils import replace
+
 
 type 
     Row = seq[char]
@@ -13,18 +14,21 @@ type
         y*: int 
 
 proc createTelemetry*(xSize: int, ySize: int): Telemetry = 
-    var row: Row
+    var row: Row = @[]
     for i in 0..<xSize:
-        row.add('0')
+        row.add(' ')
     
+    result = @[]
     for i in 0..<ySize:
         result.add(row)
 
-proc `$`*(telemetry: Telemetry): string = 
+proc `$` (telemetry: Telemetry): string = 
+    var myString = ""
     for x in 0..<telemetry.len:
         for y in 0..<telemetry[x].len:
-            result.add(telemetry[x][y])
-        result.add("\n")
+            myString.add(telemetry[x][y])
+        myString.add("\n")
+    result = myString
 
 proc `$`*(point: Point): string =
     result = "x: " & $point.x & ", y: " & $point.y 
@@ -39,54 +43,65 @@ proc xSize(telemetry: Telemetry): int =
 proc ySize(telemetry: Telemetry): int =
     result = telemetry.len
 
+iterator lines(telemetry: Telemetry): string =
+    for i in 0..<telemetry.ySize:
+        let row = telemetry[i]
+        yield cast[string](row)
+
 proc drawLineAbsolute*(telemetry: Telemetry, point1: Point, point2: Point, character: char): Telemetry = 
-    let firstPoint = if (point1.x < point2.x): point1 else: point2
-    let secondPoint = if (point1.x > point2.x): point1 else: point2
-    
-    let slope = if point2.x - point1.x != 0: 
-                    (point2.y - point1.y).float / (point2.x - point1.x).float 
-                else: 500000000.0 
-                # If x is the same, their will be a divide by zero; instead we just substitute a high number
-
-
-
-    let intercept = firstPoint.y.float - (slope*firstPoint.x.float) 
-
-    echo "Slope: " & $slope
-    echo "Intercept: " & $intercept
-
     result = telemetry
+    let startPoint = Point(x: min(point1.x, point2.x), y: min(point1.y, point2.y))
+    let endPoint = Point(x: max(point1.x, point2.x), y: max(point1.y, point2.y))
 
-    for x in firstPoint.x..secondPoint.x:
-        let y = round(x.float*slope+intercept).int
+    let slope = if endPoint.x - startPoint.x != 0: (endPoint.y-startPoint.y) / (endPoint.x-startPoint.x) else: 5000000
 
-        echo "x: " & $x
-        echo "y: " & $y
+    for y in 0..<endPoint.y: #BIG BUG HERE
+        for x in 0..<endPoint.x:
+            let idealX = round((y-startPoint.y).float * slope + startPoint.x.float)
+            let idealY = round((x-startPoint.x).float * slope + startPoint.y.float)
 
-        result = result.set(x, y, character)
+            if abs(idealY - y) < 1 or abs(idealX - x) < 1:
+                echo "X: " & $x & " , Y: " & $y
+                result = result.set(x,y,character)
+
 
 #takes percentages as inputs
-proc drawLineScaled*(telemetry: Telemetry, x1: float, y1: float, x2: float, y2: float, center: Point, zoom: float, character: char): Telemetry = 
+proc drawLineScaled*(telemetry: Telemetry, x1: float, y1: float, x2: float, y2: float, centerX: float, centerY: float, zoom: float, character: char): Telemetry = 
     let adjustedX1 = x1 * telemetry.xSize.float
     let adjustedX2 = x2 * telemetry.xSize.float
-    let adjustedY1 = (1 - y1) * telemetry.xSize.float * 0.53
-    let adjustedY2 = (1 - y2) * telemetry.xSize.float * 0.53
+    let adjustedY1 = (1 - y1) * telemetry.xSize.float
+    let adjustedY2 = (1 - y2) * telemetry.xSize.float
     
-    let centerX = (-center.x.float) * telemetry.xSize.float
-    let centerY = (-center.y.float*0.53) * telemetry.xSize.float
-        
-    var realX1 = (x1 - centerX) * zoom + telemetry.xSize / 2
-    var realX2 = (x2 - centerX) * zoom + telemetry.xSize / 2
-    var realY1 = ((y1-centerY) * zoom) + telemetry.xSize / 2
-    var realY2 = ((y2-centerY) * zoom) + telemetry.xSize / 2
+    let centerX = (-centerX.float) * telemetry.xSize.float
+    let centerY = (-centerY.float) * telemetry.xSize.float
     
-    # rewrite this in nim then you finish this method drawLine((int) real_x1,(int) real_y1,(int) real_x2,(int) real_y2, r,g,b, myChar);
-    
+    let realX1 = (adjustedX1 - centerX) * zoom - (telemetry.xSize / 2).float
+    let realX2 = (adjustedX2 - centerX) * zoom - (telemetry.xSize / 2).float 
+    let realY1 = ((adjustedY1-centerY) * zoom) - (telemetry.xSize / 2).float 
+    let realY2 = ((adjustedY2-centerY) * zoom) - (telemetry.xSize / 2).float 
 
-proc drawField(center: Point, zoom: float): Telemetry = createTelemetry(1, 1) # finish this idiot
+    let point1 = Point(x: realX1.int, y: realY1.int) 
+    let point2 = Point(x: realX2.int, y: realY2.int)
+
+    echo "point 1: " & $point1    
+    echo "point 2: " & $point2
+
+
+    result = telemetry.drawLineAbsolute(point1, point2, character)
+
+proc drawWalls(telemetry: Telemetry, zoom: float): Telemetry =
+    const wallChar = '#'
+    result = telemetry.drawLineScaled(0,0, 1, 0, 0.5,0.5, zoom, wallChar)
+    result = result.drawLineScaled(1,1, 1, 0, 0.5,0.5, zoom, wallChar)
+    result = result.drawLineScaled(0,0, 0, 1, 0.5,0.5, zoom, wallChar)
+    result = result.drawLineScaled(0,1, 1, 1, 0.5,0.5, zoom, wallChar)
+    result = result.drawLineScaled(0,1, 1, 1, 0.5,0.5, zoom, wallChar)
+
+
+proc drawField(centerX: float, centerY: float, zoom: float, xSize: int, ySize: int): Telemetry = 
+    result = createTelemetry(xSize,ySize).drawWalls(zoom)    
 
 when isMainModule:
-    discard ""
-    #echo $(createTelemetry(12, 12).drawLineScaled(
-    #   Point(x: 7, y: 2), Point(x: 4, y: 7), '|'
-    #))
+    let field = drawField(0.1,0.2, 1.0, 30, 30)  #createTelemetry(50,50)..drawLineAbsolute(Point(x:0,y:0),Point(x:50,y:50),'|')
+    for i in field.lines:
+        echo i
